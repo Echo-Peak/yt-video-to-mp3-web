855fb0327d95bba1017a4b335dcce05fca6b4920
Michael Wheeler <echopeakdev@gmail.com>
2025-10-04 16:42:46 -0400

    Update cognito auth config

--- src/index.tsx
+++ src/index.tsx
@@ -24,9 +24,9 @@ declare module "@mui/material/Button" {
 const cognitoAuthConfig = {
   authority: process.env.REACT_APP_COGNITO_AUTHORITY,
   client_id: process.env.REACT_APP_COGNITO_CLIENT_ID,
-  redirect_uri: `${window.location.origin}/`,
+  redirect_uri: process.env.REACT_APP_COGNITO_REDIRECT_URI,
   // automaticSilentRenew: true,
-  post_logout_redirect_uri: `${window.location.origin}/`,
+  post_logout_redirect_uri: process.env.REACT_APP_COGNITO_REDIRECT_URI,
   response_type: "code",
   scope: "profile openid email",
   // onSigninCallback: () => {
